08f139e3b972f1a1bdb7c746f2ddb3ea3a705b5a
paulevsGitch <paulevs@yandex.ru>
2020-09-22 22:36:55 +0300

    Cleanup

--- src/main/java/ru/betterend/mixin/client/WorldRendererMixin.java
+++ src/main/java/ru/betterend/mixin/client/WorldRendererMixin.java
@@ -2,7 +2,6 @@ package ru.betterend.mixin.client;
 
 import java.util.Random;
 
-import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;

4703d575c5761f3d39f56df6f41eb1def84d34a2
Aleksey <bulldog.seregin@gmail.com>
2020-09-30 12:25:54 +0300

    Revert "Merge branch 'master' of https://github.com/paulevsGitch/BetterEnd"
    This reverts commit f5130c4e15d7deb48a40b50f94cf0dfdb4ba19ed, reversing
    changes made to ccf99436454957b0a095d3069698b529216295a2.

--- src/main/java/ru/betterend/blocks/BlockMossyGlowshroomSapling.java
+++ src/main/java/ru/betterend/blocks/BlockMossyGlowshroomSapling.java
@@ -1,16 +1,10 @@
 package ru.betterend.blocks;
 
-import net.minecraft.world.gen.feature.Feature;
 import ru.betterend.blocks.basis.BlockFeatureSapling;
 import ru.betterend.registry.FeatureRegistry;
 
 public class BlockMossyGlowshroomSapling extends BlockFeatureSapling {
 	public BlockMossyGlowshroomSapling() {
-		super(7);
-	}
-
-	@Override
-	protected Feature<?> getFeature() {
-		return FeatureRegistry.MOSSY_GLOWSHROOM.getFeature();
+		super(FeatureRegistry.MOSSY_GLOWSHROOM, 7);
 	}
 }

101b49d0435993601e1883ab15885c9ec407c942
paulevsGitch <paulevs@yandex.ru>
2020-10-15 21:18:12 +0300

    Chorus forest ambient

--- src/main/java/ru/betterend/registry/SoundRegistry.java
+++ src/main/java/ru/betterend/registry/SoundRegistry.java
@@ -8,9 +8,11 @@ public class SoundRegistry
 {
 	// Music
 	public static final SoundEvent MUSIC_FOGGY_MUSHROOMLAND = register("music", "foggy_mushroomland");
+	public static final SoundEvent MUSIC_CHORUS_FOREST = register("music", "chorus_forest");
 	
 	// Ambient
 	public static final SoundEvent AMBIENT_FOGGY_MUSHROOMLAND = register("ambient", "foggy_mushroomland");
+	public static final SoundEvent AMBIENT_CHORUS_FOREST = register("ambient", "chorus_forest");
 	
 	// Entity
 	public static final SoundEvent ENTITY_DRAGONFLY = register("entity", "dragonfly");

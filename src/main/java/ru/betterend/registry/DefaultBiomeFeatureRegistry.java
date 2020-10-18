3583164e100305c8e5f97256f117323534b249f6
paulevsGitch <paulevs@yandex.ru>
2020-10-19 00:29:05 +0300

    Remove old class

--- src/main/java/ru/betterend/registry/DefaultBiomeFeatureRegistry.java
+++
@@ -1,5 +0,0 @@
-package ru.betterend.registry;
-
-public class DefaultBiomeFeatureRegistry {
-	//public static final EndStructureFeature MOUNTAINS = new EndStructureFeature("mountains", new StructureMountain(), Feature.RAW_GENERATION, 3, 2);
-}

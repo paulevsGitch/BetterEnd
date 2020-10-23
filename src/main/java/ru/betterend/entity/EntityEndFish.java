e76577328c382ea3b97d5ed7a23fcf450a9279c9
paulevsGitch <paulevs@yandex.ru>
2020-10-23 20:48:13 +0300

    Scale and variants

--- src/main/java/ru/betterend/entity/EntityEndFish.java
+++ src/main/java/ru/betterend/entity/EntityEndFish.java
@@ -4,14 +4,28 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.passive.SchoolingFishEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.world.World;
 
 public class EntityEndFish extends SchoolingFishEntity {
+	public static final int VARIANTS = 5;
+	private static final TrackedData<Byte> VARIANT = DataTracker.registerData(EntityEndFish.class, TrackedDataHandlerRegistry.BYTE);
+	private static final TrackedData<Byte> SCALE = DataTracker.registerData(EntityEndFish.class, TrackedDataHandlerRegistry.BYTE);
+	
 	public EntityEndFish(EntityType<EntityEndFish> entityType, World world) {
 		super(entityType, world);
+	}
+	
+	@Override
+	protected void initDataTracker() {
+		super.initDataTracker();
+		this.dataTracker.startTracking(VARIANT, (byte) this.getRandom().nextInt(VARIANTS));
+		this.dataTracker.startTracking(SCALE, (byte) this.getRandom().nextInt(255));
 	}
 
 	@Override
@@ -29,5 +43,13 @@ public class EntityEndFish extends SchoolingFishEntity {
 				.add(EntityAttributes.GENERIC_MAX_HEALTH, 2.0D)
 				.add(EntityAttributes.GENERIC_FOLLOW_RANGE, 16.0D)
 				.add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.5D);
+	}
+	
+	public int getVariant() {
+		return (int) this.dataTracker.get(VARIANT);
+	}
+	
+	public float getScale() {
+		return ((int) this.dataTracker.get(SCALE) & 255) / 512F + 0.6F;
 	}
 }
--- src/main/java/ru/betterend/entity/render/RendererEntityEndFish.java
+++ src/main/java/ru/betterend/entity/render/RendererEntityEndFish.java
@@ -1,30 +1,53 @@
 package ru.betterend.entity.render;
 
+import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.render.entity.feature.EyesFeatureRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 import ru.betterend.BetterEnd;
 import ru.betterend.entity.EntityEndFish;
 import ru.betterend.entity.model.ModelEntityEndFish;
 
 public class RendererEntityEndFish extends MobEntityRenderer<EntityEndFish, ModelEntityEndFish> {
-	private static final Identifier TEXTURE = BetterEnd.makeID("textures/entity/end_fish/end_fish_2.png");
-	private static final RenderLayer GLOW = RenderLayer.getEyes(BetterEnd.makeID("textures/entity/end_fish/end_fish_2_glow.png"));
+	private static final Identifier[] TEXTURE = new Identifier[EntityEndFish.VARIANTS];
+	private static final RenderLayer[] GLOW = new RenderLayer[EntityEndFish.VARIANTS];
 	
     public RendererEntityEndFish(EntityRenderDispatcher entityRenderDispatcher) {
         super(entityRenderDispatcher, new ModelEntityEndFish(), 0.5f);
         this.addFeature(new EyesFeatureRenderer<EntityEndFish, ModelEntityEndFish>(this) {
 			@Override
 			public RenderLayer getEyesTexture() {
-				return GLOW;
+				return GLOW[0];
+			}
+
+			@Override
+			public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, EntityEndFish entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
+				VertexConsumer vertexConsumer = vertexConsumers.getBuffer(GLOW[entity.getVariant()]);
+				this.getContextModel().render(matrices, vertexConsumer, 15728640, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
 			}
         });
     }
  
     @Override
+	protected void scale(EntityEndFish entity, MatrixStack matrixStack, float f) {
+		float scale = entity.getScale();
+		matrixStack.scale(scale, scale, scale);
+	}
+    
+    @Override
     public Identifier getTexture(EntityEndFish entity) {
-        return TEXTURE;
+        return TEXTURE[entity.getVariant()];
+    }
+    
+    static {
+    	for (int i = 0; i < EntityEndFish.VARIANTS; i++) {
+    		TEXTURE[i] = BetterEnd.makeID("textures/entity/end_fish/end_fish_" + i + ".png");
+    		GLOW[i] = RenderLayer.getEyes(BetterEnd.makeID("textures/entity/end_fish/end_fish_" + i + "_glow.png"));
+    	}
     }
 }

5f01be7b38cfd200e392228857660dc6b4425c82
Aleksey <bulldog.seregin@gmail.com>
2020-11-02 17:08:53 +0300

    Pedestals item render fix

--- src/main/java/ru/betterend/blocks/entities/render/PedestalItemRenderer.java
+++ src/main/java/ru/betterend/blocks/entities/render/PedestalItemRenderer.java
@@ -2,6 +2,7 @@ package ru.betterend.blocks.entities.render;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.RenderLayer;
@@ -9,6 +10,7 @@ import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.util.math.Vector3f;
@@ -17,6 +19,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.DyeColor;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.MathHelper;
+
 import ru.betterend.blocks.BlockProperties.PedestalState;
 import ru.betterend.blocks.EternalPedestal;
 import ru.betterend.blocks.basis.BlockPedestal;
@@ -41,6 +44,10 @@ public class PedestalItemRenderer extends BlockEntityRenderer<PedestalBlockEntit
 		BlockState state = blockEntity.getWorld().getBlockState(blockEntity.getPos());
 		ItemStack activeItem = blockEntity.getStack(0);
 		matrices.push();
+		MinecraftClient minecraft = MinecraftClient.getInstance();
+		BakedModel model = minecraft.getItemRenderer().getHeldItemModel(activeItem, blockEntity.getWorld(), null);
+		Vector3f translate = model.getTransformation().ground.translation;
+		matrices.translate(translate.getX(), translate.getY(), translate.getZ());
 		if (state.get(BlockPedestal.STATE) == PedestalState.DEFAULT) {
 			matrices.translate(0.5, 1.0, 0.5);
 		} else {
@@ -49,12 +56,11 @@ public class PedestalItemRenderer extends BlockEntityRenderer<PedestalBlockEntit
 		if (activeItem.getItem() instanceof BlockItem) {
 			matrices.scale(1.5F, 1.5F, 1.5F);
 		} else {
-			matrices.scale(1.15F, 1.15F, 1.15F);
+			matrices.scale(1.25F, 1.25F, 1.25F);
 		}
 		
 		float rotation = (blockEntity.getAge() + tickDelta) / 25.0F + 6.0F;
 		matrices.multiply(Vector3f.POSITIVE_Y.getRadialQuaternion(rotation));
-		MinecraftClient minecraft = MinecraftClient.getInstance();
 		if (state.isOf(EndBlocks.ETERNAL_PEDESTAL) && state.get(EternalPedestal.ACTIVATED)) {
 			float altitude = MathHelper.sin((blockEntity.getAge() + tickDelta) / 10.0F) * 0.1F + 0.1F;
 			matrices.translate(0.0D, altitude, 0.0D);
@@ -63,7 +69,8 @@ public class PedestalItemRenderer extends BlockEntityRenderer<PedestalBlockEntit
 			VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getBeaconBeam(BEAM_TEXTURE, true));
 			BeamRenderer.renderLightBeam(matrices, vertexConsumer, tickDelta, -y, 1024 - y, colors, 0.25F, 0.15F, 0.2F);
 		}
-		minecraft.getItemRenderer().renderItem(activeItem, ModelTransformation.Mode.GROUND, light, overlay, matrices, vertexConsumers);
+		
+		minecraft.getItemRenderer().renderItem(activeItem, ModelTransformation.Mode.GROUND, false, matrices, vertexConsumers, light, overlay, model);
 		matrices.pop();
 	}
 }

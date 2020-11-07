76804a4f09cee645dbe22b7019c630e104d99b46
paulevsGitch <paulevs@yandex.ru>
2020-11-08 01:35:16 +0300

    Merge

--- src/main/java/ru/betterend/blocks/entities/InfusionPedestalEntity.java
+++ src/main/java/ru/betterend/blocks/entities/InfusionPedestalEntity.java
@@ -1,5 +1,8 @@
 package ru.betterend.blocks.entities;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.nbt.CompoundTag;
+
 import ru.betterend.rituals.InfusionRitual;
 
 public class InfusionPedestalEntity extends PedestalBlockEntity {
@@ -9,5 +12,14 @@ public class InfusionPedestalEntity extends PedestalBlockEntity {
 	public boolean hasRitual() {
 		return this.activeRitual != null;
 	}
+	
+	@Override
+	public void fromTag(BlockState state, CompoundTag tag) {
+		super.fromTag(state, tag);
+	}
 
+	@Override
+	public CompoundTag toTag(CompoundTag tag) {
+		return super.toTag(tag);
+	}
 }

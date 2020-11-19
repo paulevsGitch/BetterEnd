fe8d2a436f7d6afe2593159e9a74446754b7181f
Aleksey <bulldog.seregin@gmail.com>
2020-11-19 23:20:46 +0300

    Try to fix End Stone Smelter

--- src/main/java/ru/betterend/client/gui/EndStoneSmelterScreenHandler.java
+++ src/main/java/ru/betterend/client/gui/EndStoneSmelterScreenHandler.java
@@ -3,6 +3,7 @@ package ru.betterend.client.gui;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry;
+
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
@@ -18,6 +19,7 @@ import net.minecraft.screen.PropertyDelegate;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.world.World;
+
 import ru.betterend.BetterEnd;
 import ru.betterend.blocks.EndStoneSmelter;
 import ru.betterend.blocks.entities.EndStoneSmelterBlockEntity;
@@ -58,6 +60,12 @@ public class EndStoneSmelterScreenHandler extends AbstractRecipeScreenHandler<In
 		for(int i = 0; i < 9; ++i) {
 			this.addSlot(new Slot(playerInventory, i, 8 + i * 18, 142));
 		}
+	}
+	
+	@Override
+	public ScreenHandlerType<?> getType() {
+		BetterEnd.LOGGER.info("End Stone Smelter: requested Handler Type: {}", HANDLER_TYPE);
+		return HANDLER_TYPE;
 	}
 
 	@Override

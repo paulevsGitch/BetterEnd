e25c3a15abafaffcc50640a5b0f98e78fcae847f
paulevsGitch <paulevs@yandex.ru>
2020-11-27 03:08:50 +0300

    Fixes

--- src/main/java/ru/betterend/blocks/BlockTenaneaSapling.java
+++ src/main/java/ru/betterend/blocks/BlockTenaneaSapling.java
@@ -1,7 +1,11 @@
 package ru.betterend.blocks;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldView;
 import net.minecraft.world.gen.feature.Feature;
 import ru.betterend.blocks.basis.BlockFeatureSapling;
+import ru.betterend.registry.EndBlocks;
 import ru.betterend.registry.EndFeatures;
 
 public class BlockTenaneaSapling extends BlockFeatureSapling {
@@ -14,8 +18,8 @@ public class BlockTenaneaSapling extends BlockFeatureSapling {
 		return EndFeatures.TENANEA.getFeature();
 	}
 	
-	/*@Override
+	@Override
 	public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
-		return world.getBlockState(pos.down()).isOf(EndBlocks.SHADOW_GRASS);
-	}*/
+		return world.getBlockState(pos.down()).isOf(EndBlocks.PINK_MOSS);
+	}
 }

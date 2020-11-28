7da08be63a4334e857221f19c0c8e7e60f530b3b
Aleksey <bulldog.seregin@gmail.com>
2020-11-29 00:22:47 +0300

    WIP: crystalite armor. New crystals icons

--- src/main/java/ru/betterend/item/model/CrystaliteArmorModel.java
+++ src/main/java/ru/betterend/item/model/CrystaliteArmorModel.java
@@ -1,12 +1,17 @@
 package ru.betterend.item.model;
 
+import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.entity.LivingEntity;
 
 public class CrystaliteArmorModel extends BipedEntityModel<LivingEntity> {
 
-	public CrystaliteArmorModel(float scale) {
-		super(scale);
+	public ModelPart helmet;
+	public ModelPart chestplate;
+	public ModelPart leggings;
+	public ModelPart boots;
+	
+	public CrystaliteArmorModel() {
+		super(1.0F);
 	}
-
 }

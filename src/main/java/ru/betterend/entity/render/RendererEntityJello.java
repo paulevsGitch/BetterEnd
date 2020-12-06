7084e294d75de0f440e28d22368241bde1dd0310
paulevsGitch <paulevs@yandex.ru>
2020-12-06 22:47:58 +0300

    Removed Jello AI

--- src/main/java/ru/betterend/entity/render/RendererEntityJello.java
+++ src/main/java/ru/betterend/entity/render/RendererEntityJello.java
@@ -13,25 +13,35 @@ import ru.betterend.entity.model.ModelEntityJello;
 public class RendererEntityJello extends MobEntityRenderer<EntityJello, ModelEntityJello> {
 	private static final Identifier TEXTURE = BetterEnd.makeID("textures/entity/jello.png");
 	private static final RenderLayer GLOW = RenderLayer.getEyes(BetterEnd.makeID("textures/entity/jello_glow.png"));
-	
-    public RendererEntityJello(EntityRenderDispatcher entityRenderDispatcher) {
-        super(entityRenderDispatcher, new ModelEntityJello(), 0.5f);
-        this.addFeature(new EyesFeatureRenderer<EntityJello, ModelEntityJello>(this) {
+
+	public RendererEntityJello(EntityRenderDispatcher entityRenderDispatcher) {
+		super(entityRenderDispatcher, new ModelEntityJello(), 0.5f);
+		this.addFeature(new EyesFeatureRenderer<EntityJello, ModelEntityJello>(this) {
 			@Override
 			public RenderLayer getEyesTexture() {
 				return GLOW;
 			}
-        });
-    }
- 
-    @Override
+		});
+	}
+
+	@Override
 	protected void scale(EntityJello entity, MatrixStack matrixStack, float f) {
 		float scale = entity.getScale();
 		matrixStack.scale(scale, scale, scale);
 	}
-    
-    @Override
-    public Identifier getTexture(EntityJello entity) {
-        return TEXTURE;
-    }
+
+	@Override
+	public Identifier getTexture(EntityJello entity) {
+		return TEXTURE;
+	}
+
+	/*@Override
+	protected void setupTransforms(EntityJello squidEntity, MatrixStack matrixStack, float f, float g, float h) {
+		float i = MathHelper.lerp(h, squidEntity.prevTiltAngle, squidEntity.tiltAngle);
+		float j = MathHelper.lerp(h, squidEntity.prevRollAngle, squidEntity.rollAngle);
+		matrixStack.translate(0, -0.5F, 0);
+		matrixStack.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(180.0F - g));
+		matrixStack.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(i));
+		matrixStack.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(j));
+	}*/
 }

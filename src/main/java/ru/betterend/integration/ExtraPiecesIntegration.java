2a2641eea373d9798aaeaa35c24dadd84706fd1d
paulevsGitch <paulevs@yandex.ru>
2021-01-05 16:33:19 +0300

    Temporary disabled WIP integrations

---
+++ src/main/java/ru/betterend/integration/ExtraPiecesIntegration.java
@@ -0,0 +1,17 @@
+package ru.betterend.integration;
+
+public class ExtraPiecesIntegration extends ModIntegration {
+	public ExtraPiecesIntegration() {
+		super("extrapieces");
+	}
+
+	@Override
+	public void register() {
+		//Class<?> pieceSets = getClass("com.shnupbups.extrapieces.core.PieceSets");
+	}
+
+	@Override
+	public void addBiomes() {
+		
+	}
+}

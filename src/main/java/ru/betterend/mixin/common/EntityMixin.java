25efe551b2354089846fd4524a7b441f20d4cd13
paulevsGitch <paulevs@yandex.ru>
2021-01-13 17:10:58 +0300

    Small cleanup

--- src/main/java/ru/betterend/mixin/common/EntityMixin.java
+++ src/main/java/ru/betterend/mixin/common/EntityMixin.java
@@ -1,7 +1,5 @@
 package ru.betterend.mixin.common;
 
-import java.util.Map;
-
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;

f62f88e4bd5cd4dce755dbebea3117855733097c
paulevsGitch <paulevs@yandex.ru>
2021-01-23 01:37:52 +0300

    Water color option, disabling color if sodium is installed

--- src/main/java/ru/betterend/client/ClientOptions.java
+++ src/main/java/ru/betterend/client/ClientOptions.java
@@ -1,16 +1,22 @@
 package ru.betterend.client;
 
+import net.fabricmc.loader.api.FabricLoader;
 import ru.betterend.config.Configs;
 
 public class ClientOptions {
 	private static boolean customSky;
 	private static boolean useFogDensity;
 	private static boolean blendBiomeMusic;
+	private static boolean sulfurWaterColor;
 	
 	public static void init() {
-		setCustomSky(Configs.CLENT_CONFIG.getBooleanRoot("customSky", true));
-		setUseFogDensity(Configs.CLENT_CONFIG.getBooleanRoot("useFogDensity", true));
-		setBlendBiomeMusic(Configs.CLENT_CONFIG.getBooleanRoot("blendBiomeMusic", true));
+		customSky = Configs.CLENT_CONFIG.getBooleanRoot("customSky", true);
+		useFogDensity = Configs.CLENT_CONFIG.getBooleanRoot("useFogDensity", true);
+		blendBiomeMusic = Configs.CLENT_CONFIG.getBooleanRoot("blendBiomeMusic", true);
+		sulfurWaterColor = Configs.CLENT_CONFIG.getBooleanRoot("sulfurWaterColor", true);
+		if (sulfurWaterColor) {
+			sulfurWaterColor = !FabricLoader.getInstance().isModLoaded("sodium");
+		}
 		Configs.CLENT_CONFIG.saveChanges();
 	}
 
@@ -36,5 +42,13 @@ public class ClientOptions {
 
 	public static void setBlendBiomeMusic(boolean blendBiomeMusic) {
 		ClientOptions.blendBiomeMusic = blendBiomeMusic;
+	}
+	
+	public static boolean useSulfurWaterColor() {
+		return sulfurWaterColor;
+	}
+
+	public static void setSulfurWaterColor(boolean sulfurWaterColor) {
+		ClientOptions.sulfurWaterColor = sulfurWaterColor && !FabricLoader.getInstance().isModLoaded("sodium");
 	}
 }

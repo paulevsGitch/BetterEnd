6bdc2fa7c7edb4242fa960a8a206c36f550ea62a
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-03-17 20:33:56 +0100

    fix knockback resistance not being applied

--- src/main/java/ru/betterend/item/EndArmorItem.java
+++ src/main/java/ru/betterend/item/EndArmorItem.java
@@ -1,17 +1,49 @@
 package ru.betterend.item;
 
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.Item;
+import ru.betterend.mixin.common.ArmorItemAccessor;
 import ru.betterend.patterns.Patterned;
 import ru.betterend.patterns.Patterns;
+
+import java.util.UUID;
 
 public class EndArmorItem extends ArmorItem implements Patterned {
 	public EndArmorItem(ArmorMaterial material, EquipmentSlot slot, Item.Settings settings) {
 		super(material, slot, settings);
+
+		addKnockbackResistance((ArmorItemAccessor) this, slot, this.knockbackResistance);
 	}
-	
+
+	/** Ensures knockback resistance is actually applied */
+	private static void addKnockbackResistance(ArmorItemAccessor accessor, EquipmentSlot slot, double knockbackResistance) {
+		if (knockbackResistance == 0) {
+			return;
+		}
+
+		Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers = accessor.getAttributeModifiers();
+
+		// In case Mojang or anyone else decided to fix this
+		if (attributeModifiers.keys().contains(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE)) {
+			return;
+		}
+
+		UUID uuid = accessor.getModifiers()[slot.getEntitySlotId()];
+
+		// Rebuild attributeModifiers to include knockback resistance
+		ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
+		builder.putAll(attributeModifiers);
+		builder.put(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, new EntityAttributeModifier(uuid, "Armor knockback resistance", knockbackResistance, EntityAttributeModifier.Operation.ADDITION));
+		accessor.setAttributeModifiers(builder.build());
+	}
+
 	@Override
 	public String getModelPattern(String name) {
 		return Patterns.createJson(Patterns.ITEM_GENERATED, name);
---
+++ src/main/java/ru/betterend/mixin/common/ArmorItemAccessor.java
@@ -0,0 +1,22 @@
+package ru.betterend.mixin.common;
+
+import com.google.common.collect.Multimap;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.item.ArmorItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.UUID;
+
+@Mixin(ArmorItem.class)
+public interface ArmorItemAccessor {
+	@Accessor("MODIFIERS")
+	UUID[] getModifiers();
+
+	@Accessor("attributeModifiers")
+	Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers();
+
+	@Accessor("attributeModifiers")
+	void setAttributeModifiers(Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers);
+}

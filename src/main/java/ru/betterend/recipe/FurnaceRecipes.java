58a29704436011329e4cef63dc5083ff44aa142f
paulevsGitch <paulevs@yandex.ru>
2021-03-17 17:17:54 +0300

    Missing recipe

--- src/main/java/ru/betterend/recipe/FurnaceRecipes.java
+++ src/main/java/ru/betterend/recipe/FurnaceRecipes.java
@@ -15,5 +15,6 @@ public class FurnaceRecipes {
 		FurnaceRecipe.make("slime_ball", EndBlocks.JELLYSHROOM_CAP_PURPLE, Items.SLIME_BALL).build();
 		FurnaceRecipe.make("menger_sponge", EndBlocks.MENGER_SPONGE_WET, EndBlocks.MENGER_SPONGE).build();
 		FurnaceRecipe.make("chorus_mushroom", EndItems.CHORUS_MUSHROOM_RAW, EndItems.CHORUS_MUSHROOM_COOKED).buildFoodlike();
+		FurnaceRecipe.make("bolux_mushroom", EndBlocks.BOLUX_MUSHROOM, EndItems.BOLUX_MUSHROOM_COOKED).buildFoodlike();
 	}
 }

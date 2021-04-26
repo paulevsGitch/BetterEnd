d897bb09d8f4de08c56b1ed58a0672da817df6eb
Aleksey <bulldog.seregin@gmail.com>
2021-04-26 09:51:26 +0300

    Cleanup

--- src/main/java/ru/betterend/mixin/common/TagLoaderMixin.java
+++ src/main/java/ru/betterend/mixin/common/TagLoaderMixin.java
@@ -1,26 +1,17 @@
 package ru.betterend.mixin.common;
 
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-import java.util.function.Supplier;
-
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.Tag;
+import net.minecraft.tags.TagLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.At.Shift;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import com.google.common.collect.Maps;
-
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.ResourceManager;
-import net.minecraft.tags.Tag;
-import net.minecraft.tags.TagLoader;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import ru.betterend.util.TagHelper;
+
+import java.util.Map;
+import java.util.concurrent.Executor;
+import java.util.function.Supplier;
 
 @Mixin(TagLoader.class)
 public class TagLoaderMixin {

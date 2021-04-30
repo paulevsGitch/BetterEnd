086657198ebef6e3069d732695407d44f52b7de4
Aleksey <bulldog.seregin@gmail.com>
2021-04-30 13:28:40 +0300

    Resolve conflicts

--- src/main/java/ru/betterend/patterns/BlockPatterned.java
+++ src/main/java/ru/betterend/patterns/BlockPatterned.java
@@ -1,19 +1,3 @@
-<<<<<<< Updated upstream
-package ru.betterend.patterns;
-
-import java.io.Reader;
-
-import net.minecraft.resources.ResourceLocation;
-
-public interface BlockPatterned extends Patterned {
-	default String getStatesPattern(Reader data) {
-		return null;
-	}
-	default ResourceLocation statePatternId() {
-		return null;
-	}
-}
-=======
 package ru.betterend.patterns;
 
 import java.io.Reader;
@@ -31,4 +15,3 @@ public interface BlockPatterned extends Patterned {
 		return null;
 	}
 }
->>>>>>> Stashed changes

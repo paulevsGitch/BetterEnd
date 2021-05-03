94b3fe4b5c4c03c35f3dc5b50092856956cd5b37
paulevsGitch <paulevs@yandex.ru>
2021-05-04 01:48:17 +0300

    More Nourish tags

--- src/main/java/ru/betterend/integration/NourishIntegration.java
+++ src/main/java/ru/betterend/integration/NourishIntegration.java
@@ -17,9 +17,36 @@ public class NourishIntegration extends ModIntegration {
 		Tag.Named<Item> protein = getItemTag("protein");
 		Tag.Named<Item> sweets = getItemTag("sweets");
 		
-		TagHelper.addTag(fats, EndItems.END_FISH_RAW, EndItems.END_FISH_COOKED);
-		TagHelper.addTag(fruit, EndItems.SHADOW_BERRY_RAW, EndItems.SHADOW_BERRY_COOKED, EndItems.BLOSSOM_BERRY, EndItems.SHADOW_BERRY_JELLY, EndItems.SWEET_BERRY_JELLY);
-		TagHelper.addTag(protein, EndItems.END_FISH_RAW, EndItems.END_FISH_COOKED);
-		TagHelper.addTag(sweets, EndItems.SHADOW_BERRY_JELLY, EndItems.SWEET_BERRY_JELLY);
+		TagHelper.addTag(
+			fats,
+			EndItems.END_FISH_RAW,
+			EndItems.END_FISH_COOKED
+		);
+		TagHelper.addTag(
+			fruit,
+			EndItems.SHADOW_BERRY_RAW,
+			EndItems.SHADOW_BERRY_COOKED,
+			EndItems.BLOSSOM_BERRY,
+			EndItems.SHADOW_BERRY_JELLY, 
+			EndItems.SWEET_BERRY_JELLY,
+			EndItems.AMBER_ROOT_RAW,
+			EndItems.CHORUS_MUSHROOM_RAW,
+			EndItems.CHORUS_MUSHROOM_COOKED,
+			EndItems.BOLUX_MUSHROOM_COOKED
+		);
+		TagHelper.addTag(
+			protein,
+			EndItems.END_FISH_RAW,
+			EndItems.END_FISH_COOKED,
+			EndItems.CHORUS_MUSHROOM_COOKED,
+			EndItems.BOLUX_MUSHROOM_COOKED,
+			EndItems.CAVE_PUMPKIN_PIE
+		);
+		TagHelper.addTag(
+			sweets,
+			EndItems.SHADOW_BERRY_JELLY,
+			EndItems.SWEET_BERRY_JELLY,
+			EndItems.CAVE_PUMPKIN_PIE
+		);
 	}
 }

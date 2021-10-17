6148bdc451493d23f06847747a83b6d28ac7888a
Frank Bauer <developer@ambertation.de>
2021-10-17 18:45:35 +0200

    Removed build-dependency to IPN

---
+++ src/main/java/org/anti_ad/mc/ipn/api/IPNIgnore.java
@@ -0,0 +1,8 @@
+package org.anti_ad.mc.ipn.api;
+
+import java.lang.annotation.*;
+
+// Included from "Inventory Profiles Next" (https://github.com/blackd/Inventory-Profiles)
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface IPNIgnore {}

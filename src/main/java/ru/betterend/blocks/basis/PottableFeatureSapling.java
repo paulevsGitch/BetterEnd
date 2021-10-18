da8813ba06dafe744ec02081e94cd8d5e09a03b4
Frank <developer@ambertation.de>
2021-10-18 20:10:07 +0200

    Using Auto-Tag feature for saplings (resolves #303)

--- src/main/java/ru/betterend/blocks/basis/PottableFeatureSapling.java
+++ src/main/java/ru/betterend/blocks/basis/PottableFeatureSapling.java
@@ -1,13 +1,11 @@
 package ru.betterend.blocks.basis;
 
-import ru.bclib.api.TagAPI;
 import ru.bclib.blocks.FeatureSaplingBlock;
 import ru.betterend.interfaces.PottablePlant;
 
 public abstract class PottableFeatureSapling extends FeatureSaplingBlock implements PottablePlant {
 	public PottableFeatureSapling() {
 		super();
-		TagAPI.addTags(this, TagAPI.BLOCK_SAPLINGS);
 	}
 	
 	public PottableFeatureSapling(int light) {

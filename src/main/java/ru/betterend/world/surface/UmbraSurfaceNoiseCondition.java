4b27f00eb601a5ace57c10cfa2643c17b0050274
Frank <developer@ambertation.de>
2022-01-20 19:39:49 +0100

    Added Codecs for SurfaceRule Objects (paulevsGitch/BetterNether#503)

--- src/main/java/ru/betterend/world/surface/UmbraSurfaceNoiseCondition.java
+++ src/main/java/ru/betterend/world/surface/UmbraSurfaceNoiseCondition.java
@@ -1,14 +1,20 @@
 package ru.betterend.world.surface;
 
+import com.mojang.serialization.Codec;
+import net.minecraft.core.Registry;
 import ru.bclib.interfaces.NumericProvider;
 import ru.bclib.mixin.common.SurfaceRulesContextAccessor;
 import ru.bclib.util.MHelper;
+import ru.betterend.BetterEnd;
 import ru.betterend.noise.OpenSimplexNoise;
 
 /**
  * Noise source that returns a value in [0, 4]
  */
 public class UmbraSurfaceNoiseCondition implements NumericProvider {
+	public static final UmbraSurfaceNoiseCondition DEFAULT = new UmbraSurfaceNoiseCondition();
+	public static final Codec<UmbraSurfaceNoiseCondition> CODEC = Codec.BYTE.fieldOf("umbra_srf").xmap((obj)->DEFAULT, obj -> (byte)0).codec();
+
 	private static final OpenSimplexNoise NOISE = new OpenSimplexNoise(1512);
 
 	@Override
@@ -25,5 +31,14 @@ public class UmbraSurfaceNoiseCondition implements NumericProvider {
 		if (value > -0.15) return 2;
 		if (value > -0.4) return 3;
 		return 4;
+	}
+
+	@Override
+	public Codec<? extends NumericProvider> pcodec() {
+		return CODEC;
+	}
+
+	static {
+		Registry.register(NumericProvider.NUMERIC_PROVIDER , BetterEnd.makeID("umbra_srf"), UmbraSurfaceNoiseCondition.CODEC);
 	}
 }

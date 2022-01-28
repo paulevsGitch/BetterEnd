4e008ab20892e28400cfbc9f856b2a7ce1e36d8b
Frank <developer@ambertation.de>
2022-01-28 17:45:26 +0100

    Using ThreadLocal storage

--- src/main/java/ru/betterend/util/GlobalState.java
+++ src/main/java/ru/betterend/util/GlobalState.java
@@ -2,14 +2,9 @@ package ru.betterend.util;
 
 import net.minecraft.core.BlockPos.MutableBlockPos;
 
-import java.util.concurrent.ConcurrentHashMap;
-
 public class GlobalState {
-	private static final ConcurrentHashMap<Thread, GlobalState> statePool = new ConcurrentHashMap<>();
-	public static void clearStatePool(){
-		statePool.clear();
-	}
-	public static GlobalState stateForThread() { return statePool.computeIfAbsent(Thread.currentThread(), t-> new GlobalState()); }
+	private static final ThreadLocal<GlobalState> STATE = ThreadLocal.withInitial(()->new GlobalState());
+	public static GlobalState stateForThread() { return STATE.get(); }
 
 	public final MutableBlockPos POS = new MutableBlockPos();
 }
